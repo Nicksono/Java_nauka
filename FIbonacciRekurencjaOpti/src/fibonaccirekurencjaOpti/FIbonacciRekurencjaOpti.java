c1d0258b7c705ef18ae5b22e1446228d327be28a
Krzysztof Zieliński <45273306+Krzyziet@users.noreply.github.com>
2018-11-28 19:53:10 +0100

    Update FIbonacciRekurencjaOpti.java

--- FIbonacciRekurencjaOpti/src/fibonaccirekurencjaOpti/FIbonacciRekurencjaOpti.java
+++ FIbonacciRekurencjaOpti/src/fibonaccirekurencjaOpti/FIbonacciRekurencjaOpti.java
@@ -1,24 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fibonaccirekurencjaOpti;
 
-/**
- *
- * @author Krzysiek
- */
 public class FIbonacciRekurencjaOpti {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
-        // TODO code application logic here
-    Fibonacci liczba = new Fibonacci();    // utworzenie OBIEKTU liczba 
-    System.out.println("50 wyraz ciągu Fibonacciego to : " + liczba.obiektfib(50));
+        Fibonacci liczba = new Fibonacci();    // utworzenie OBIEKTU liczba 
+        System.out.println("50 wyraz ciągu Fibonacciego to : " + liczba.obiektfib(50));
     }
 }
 class Fibonacci    // Stworzenie Klasy FIbonacci która może mieć w sobie wiele metod ( funkcji)
@@ -32,15 +20,14 @@ class Fibonacci    // Stworzenie Klasy FIbonacci która może mieć w sobie wiel
             {
                 if(wyraz == 0) //sprawdzamy czy wyraz nie jest 0 
                 {
-                    tablica[wyraz]=0; // jesli tak zwracamy 0
-                    return 0;
+                    tablica[wyraz]=0; 
+                    return 0; // jesli tak zwracamy 0
                 }
                 if(wyraz == 1) //sprawdzam czy wyraz nie jest 1 
                 {
-                    tablica[wyraz]=1; // jesli tak zwracamy 1
-                    return 1;
-                }
-                tablica[wyraz]=tablica[wyraz-1]+tablica[wyraz-2]; // komórka jest pusta więc nadajemy jej wartość              
+                    tablica[wyraz]=1; 
+                    return 1; // jesli tak zwracamy 1
+                }            
             }
             return tablica[wyraz]; //zwracamy wartosc 2 wyrazu
         }
@@ -50,6 +37,6 @@ class Fibonacci    // Stworzenie Klasy FIbonacci która może mieć w sobie wiel
             }
         else
             tablica[wyraz]=tablica[wyraz-1]+tablica[wyraz-2]; // komorka jest pusta więc nadajemy jej wartość 
-                return obiektfib(wyraz-1)+obiektfib(wyraz-2); //rekurencja odnosimy cały czas cofamy się do tego miejsca tworząc drzewo !!
+        return obiektfib(wyraz-1)+obiektfib(wyraz-2); //rekurencja odnosimy cały czas cofamy się do tego miejsca tworząc drzewo !!
     }
 } 
